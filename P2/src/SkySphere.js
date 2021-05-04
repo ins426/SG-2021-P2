2b1bf2f69ce08d38ce63e64b5062189dde08f3a8
Ines Nieto <insherondale26@gmail.com>
2021-05-04 17:12:31 +0200

    Añadido escenario y gato de pruebas

--- P2/src/SkySphere.js
+++ P2/src/SkySphere.js
@@ -7,17 +7,15 @@ class SkySphere extends THREE.Object3D {
         super();
 
        // this.cargarTextura();
-       var loader = new THREE.TextureLoader();
-       loader.load('../imgs/mar.jpg');
-       var material = new THREE.MeshBasicMaterial({map:texture,overdraw:0.5});
+       var texture = new THREE.TextureLoader().load('../imgs/mar.jpg');
+       this.materialGround = new THREE.MeshPhongMaterial ({map: texture, side: THREE.BackSide});
 
-        var mundoGeometry = new THREE.SphereBufferGeometry(30,32,32);
+        var mundoGeometry = new THREE.SphereBufferGeometry(100,32,32);
         //var mundoMaterial = new THREE.MeshNormalMaterial({map: material, side: THREE.BackSide});
 
-        var texture = new THREE.TextureLoader().load('../imgs/mar.jpg');
-        this.materialGround = new THREE.MeshPhongMaterial ({map: texture});
+        
 
-        var mundo = new THREE.Mesh(mundoGeometry,material);
+        var mundo = new THREE.Mesh(mundoGeometry,this.materialGround);
 
         this.add(mundo);
     }
